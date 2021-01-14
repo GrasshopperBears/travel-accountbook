ccd00e21426e4a3925812a0ea7275dba0951c03c
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-01-15 04:02:41 +0900

    base url 변경

--- client/src/services/api-request.js
+++ client/src/services/api-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: `${process.env.REACT_APP_SERVER_HOST}/api`,
 });
 
 apiRequest.interceptors.request.use(
