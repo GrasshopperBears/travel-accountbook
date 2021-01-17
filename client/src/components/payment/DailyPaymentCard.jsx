8b8ca079c77b3cdf19c171ae1de9f5d4607caafb
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-01-18 01:52:39 +0900

    FE(design): 날짜와 총액에 색깔 표시

--- client/src/components/payment/DailyPaymentCard.jsx
+++ client/src/components/payment/DailyPaymentCard.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Card, List, Row, Col } from 'antd';
 import moment from 'moment';
+import styled from 'styled-components';
 import PaymentInfo from './PaymentInfo';
+import 'moment/locale/ko';
 
 const DailyPaymentCard = ({ paymentsInDay, onClickModify }) => {
   const [dailyTotal, setDailyTotal] = useState(0);
@@ -14,30 +16,48 @@ const DailyPaymentCard = ({ paymentsInDay, onClickModify }) => {
   }, [paymentsInDay]);
 
   return (
-    <Card
-      title={<DailyHeader date={paymentsInDay[0].date} totalAmount={dailyTotal} />}
-      bordered={false}
-      style={{ marginBottom: '2rem' }}
-    >
+    <DailyCard title={<DailyHeader date={paymentsInDay[0].date} totalAmount={dailyTotal} />} bordered={false}>
       <List
         dataSource={paymentsInDay}
         renderItem={(payment) => (
           <PaymentInfo key={payment.id} info={payment} onClickModify={onClickModify} />
         )}
       />
-    </Card>
+    </DailyCard>
   );
 };
 
 const DailyHeader = ({ date, totalAmount }) => {
   return (
-    <Row style={{ width: '100%' }}>
-      <Col span={12}>{moment(date).format('MM월 DD일')}</Col>
-      <Col span={12} style={{ textAlign: 'right' }}>
+    <CardHeaderRow>
+      <CardHeaderCol flex='1' style={{ backgroundColor: '#41945B' }}>
+        {moment(date).locale('ko').format('MM월 DD일(ddd)')}
+      </CardHeaderCol>
+      <CardHeaderCol flex='3' style={{ textAlign: 'right', backgroundColor: '#DEA3A2' }}>
         {totalAmount.toLocaleString()}원
-      </Col>
-    </Row>
+      </CardHeaderCol>
+    </CardHeaderRow>
   );
 };
+
+const DailyCard = styled(Card)`
+  margin-bottom: 2rem;
+  .ant-card-head {
+    min-height: fit-content;
+    padding: 0;
+  }
+  .ant-card-head-title {
+    padding: 0;
+  }
+  .ant-card-body {
+    padding-top: 9px;
+  }
+`;
+const CardHeaderRow = styled(Row)`
+  width: 100%;
+`;
+const CardHeaderCol = styled(Col)`
+  padding: 10px 22px;
+`;
 
 export default DailyPaymentCard;
