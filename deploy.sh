91d1123e1b09da71dfaa317463a36634b08477f8
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-01-18 02:21:21 +0900

    add update shell

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/travel-accountbook
+git fetch origin
+SERVER_COMPARE=$(git diff FETCH_HEAD..HEAD server)
+CLIENT_COMPARE=$(git diff FETCH_HEAD..HEAD client)
+git merge FETCH_HEAD
+if [ -n "$SERVER_COMPARE" ]; then
+  cd server
+  npm i
+  pm2 restart www
+  echo "---------SERVER UPDATE FINISHED---------"
+  cd ..
+fi
+if [ -n "$CLIENT_COMPARE" ]; then
+  cd client
+  npm i
+  npm run build
+  echo "---------BUILD FINISHED---------"
+fi
+echo "---------DEPLOY FINISHED---------"
